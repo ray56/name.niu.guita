9b302149270df2e0dc68562e610e8ad0dc5b320b
ray <ray56hell@gmail.com>
2011-04-16 01:29:05 +0800

    add src_sandbox

---
+++ src_sandbox/name.niu.guita.sandbox.export_excel/src/name/niu/guita/sandbox/dataObject/TestCaseDescription.java
@@ -0,0 +1,52 @@
+package name.niu.guita.sandbox.dataObject;
+
+public class TestCaseDescription {
+
+	public TestCaseDescription(String no, String testCaseId, String operations,
+			String exceptedResult) {
+		this.no = no;
+		this.testCaseId = testCaseId;
+		this.operations = operations;
+		this.exceptedResult = exceptedResult;
+	}
+	public TestCaseDescription() {
+
+	}
+	public String getNo() {
+		return no;
+	}
+
+	public void setNo(String no) {
+		this.no = no;
+	}
+
+	public String getTestCaseId() {
+		return testCaseId;
+	}
+
+	public void setTestCaseId(String testCaseId) {
+		this.testCaseId = testCaseId;
+	}
+
+	public String getOperations() {
+		return operations;
+	}
+
+	public void setOperations(String operations) {
+		this.operations = operations;
+	}
+
+	public String getExceptedResult() {
+		return exceptedResult;
+	}
+
+	public void setExceptedResult(String exceptedResult) {
+		this.exceptedResult = exceptedResult;
+	}
+
+	private String no;
+	private String testCaseId;
+	private String operations;
+	private String exceptedResult;
+
+}
---
+++ src_sandbox/name.niu.guita.sandbox.using_uisut/src/name/niu/guita/sandbox/using_uisut/Perspective.java
@@ -0,0 +1,10 @@
+package name.niu.guita.sandbox.using_uisut;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
