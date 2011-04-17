df442355cbef5a7a828d95907c9ffd5c4dc3ac74
ray <ray56hell@gmail.com>
2011-04-17 22:19:10 +0800

    [Refactor] separate Action and Algorithm/Process

---
+++ src/name.niu.guita.uitf.xlsgen/src/name/niu/guita/uitf/xlsgen/actions/GenExcelAction.java
@@ -0,0 +1,47 @@
+package name.niu.guita.uitf.xlsgen.actions;
+
+import java.util.Iterator;
+import java.util.Locale;
+
+import javax.swing.JOptionPane;
+
+import name.niu.guita.uitf.xlsgen.algorithms.GenExcelAlgorithm;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class GenExcelAction extends AbstractHandler {
+	
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		ISelection sel = HandlerUtil.getCurrentSelectionChecked(event) ;
+		if ( sel instanceof IFile ) {
+			IFile f = (IFile) sel ;
+			System.out.println( f.getFullPath() ) ;
+		} else if ( sel instanceof IStructuredSelection ) {
+			Iterator elements = ((IStructuredSelection) sel).iterator();
+			while ( elements.hasNext()){
+				Object ele = elements.next() ;
+				if (ele instanceof IAdaptable ) {
+					IAdaptable adaptable = (IAdaptable) ele ;
+					IFile f = (IFile) adaptable.getAdapter(IFile.class);
+					if ( f.getFileExtension().toLowerCase(Locale.ENGLISH).equals( "uitf" ) ) {
+						GenExcelAlgorithm.GenScript( f) ;
+					} else {
+						JOptionPane.showMessageDialog(null, "Select *.UITF file");
+					}
+					
+				}
+			}
+		}
+		return null;
+	}
+
+}
