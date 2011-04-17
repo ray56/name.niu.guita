f017a52180ac930ef8ec69978394ca250d3fbfc7
ray <ray56hell@gmail.com>
2011-04-17 20:41:49 +0800

    [Implement Function] test script generation.

--- src/name.niu.guita.uitf.scriptgen/src/name/niu/guita/uitf/scriptgen/actions/GenerateScriptAction.java
+++ src/name.niu.guita.uitf.scriptgen/src/name/niu/guita/uitf/scriptgen/actions/GenerateScriptAction.java
@@ -1,46 +1,44 @@
 package name.niu.guita.uitf.scriptgen.actions;
 
+import java.util.Iterator;
+
+import name.niu.guita.uitf.scriptgen.algorithms.GenScriptAlgorithm;
+
+import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
 
-public class GenerateScriptAction implements IHandler {
-
-	@Override
-	public void addHandlerListener(IHandlerListener handlerListener) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-
-	}
+public class GenerateScriptAction extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+		ISelection sel = HandlerUtil.getCurrentSelectionChecked(event) ;
+		if ( sel instanceof IFile ) {
+			IFile f = (IFile) sel ;
+			System.out.println( f.getFullPath() ) ;
+		} else if ( sel instanceof IStructuredSelection ) {
+			Iterator elements = ((IStructuredSelection) sel).iterator();
+			while ( elements.hasNext()){
+				Object ele = elements.next() ;
+				if (ele instanceof IAdaptable ) {
+					IAdaptable adaptable = (IAdaptable) ele ;
+					IFile f = (IFile) adaptable.getAdapter(IFile.class);
+					GenScriptAlgorithm.GenScript( f) ;
+				}
+			}
+				
+			
+		}
 		return null;
 	}
+	
 
-	@Override
-	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isHandled() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void removeHandlerListener(IHandlerListener handlerListener) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
