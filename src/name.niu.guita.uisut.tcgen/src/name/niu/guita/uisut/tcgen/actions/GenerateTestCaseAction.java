bf065ff6f98d2fd3724283b7f2331b7eb953c858
ray <ray56hell@gmail.com>
2011-04-19 16:07:06 +0800

    [Enhance UI] Select In Navigation and gen

--- src/name.niu.guita.uisut.tcgen/src/name/niu/guita/uisut/tcgen/actions/GenerateTestCaseAction.java
+++ src/name.niu.guita.uisut.tcgen/src/name/niu/guita/uisut/tcgen/actions/GenerateTestCaseAction.java
@@ -1,9 +1,19 @@
 package name.niu.guita.uisut.tcgen.actions;
 
+import java.util.Iterator;
+import java.util.Locale;
+
+import javax.swing.JOptionPane;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -12,17 +22,51 @@ public class GenerateTestCaseAction extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		Shell shell = HandlerUtil.getActiveWorkbenchWindow(event).getShell() ;
-		FileDialog fileDialog = new FileDialog(shell);
-		fileDialog.setText("Select uisut File");
-		fileDialog.setFilterExtensions(new String[] { "*.uisut" });
-		fileDialog.setFilterNames(new String[] { "Uitf files(*.uisut)" });
-		String source_uisut_file = fileDialog.open();
-		System.out.println(source_uisut_file);
-		String target_uitf_file = URI.createFileURI( source_uisut_file ).
-				trimFileExtension().appendFileExtension("uitf").toFileString();
-		GenerationTestCaseAlgorithm.genAlgorithm(source_uisut_file, target_uitf_file) ;
+//		Shell shell = HandlerUtil.getActiveWorkbenchWindow(event).getShell() ;
+//		FileDialog fileDialog = new FileDialog(shell);
+//		fileDialog.setText("Select uisut File");
+//		fileDialog.setFilterExtensions(new String[] { "*.uisut" });
+//		fileDialog.setFilterNames(new String[] { "Uitf files(*.uisut)" });
+//		String source_uisut_file = fileDialog.open();
+//		System.out.println(source_uisut_file);
+//		String target_uitf_file = URI.createFileURI( source_uisut_file ).
+//				trimFileExtension().appendFileExtension("uitf").toFileString();
+//		GenerationTestCaseAlgorithm.genAlgorithm(source_uisut_file, target_uitf_file) ;
 		
+		ISelection sel = HandlerUtil.getCurrentSelectionChecked(event) ;
+		if ( sel instanceof IFile ) {
+			IFile f = (IFile) sel ;
+			System.out.println( f.getFullPath() ) ;
+		} else if ( sel instanceof IStructuredSelection ) {
+			Iterator elements = ((IStructuredSelection) sel).iterator();
+			while ( elements.hasNext()){
+				Object ele = elements.next() ;
+				if (ele instanceof IAdaptable ) {
+					IAdaptable adaptable = (IAdaptable) ele ;
+					IFile f = (IFile) adaptable.getAdapter(IFile.class);			
+					if ( f.getFileExtension().toLowerCase(Locale.ENGLISH).equals( "uisut" ) ) {
+						//
+						String source_uisut_file = f.getLocation().toString() ;
+						String target_uitf_file = source_uisut_file
+								.substring(0, source_uisut_file.length()-"uisut".length() ) 
+								+ "uitf";
+						GenerationTestCaseAlgorithm.genAlgorithm(source_uisut_file, target_uitf_file);
+						
+						try {
+							f.getParent().refreshLocal(0, null);
+						} catch (CoreException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					} else {
+						JOptionPane.showMessageDialog(null, "Select *.UISUT file");
+						
+					}
+				}
+			}
+				
+			
+		}
 		return null;
 	}
 
