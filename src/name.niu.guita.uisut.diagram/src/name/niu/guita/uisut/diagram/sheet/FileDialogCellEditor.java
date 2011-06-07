266a4bca35fcf57fb65d812929d79ff60e103b23
ray <ray56hell@gmail.com>
2011-06-08 00:03:50 +0800

    [UI] set pic of State: two button: clear and openFileDialog

--- src/name.niu.guita.uisut.diagram/src/name/niu/guita/uisut/diagram/sheet/FileDialogCellEditor.java
+++ src/name.niu.guita.uisut.diagram/src/name/niu/guita/uisut/diagram/sheet/FileDialogCellEditor.java
@@ -8,7 +8,10 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.FileDialog;
 
-public class FileDialogCellEditor extends DialogCellEditor  {
+import org.eclipse.gmf.runtime.common.ui.services.properties.extended.IPropertyAction;
+import org.eclipse.gmf.runtime.common.ui.services.properties.extended.MultiButtonCellEditor ;
+
+public class FileDialogCellEditor extends MultiButtonCellEditor/*DialogCellEditor*/  {
 
 	 public FileDialogCellEditor(Composite parent)
 	 {
@@ -41,7 +44,7 @@ public class FileDialogCellEditor extends DialogCellEditor  {
 	        Button result = new Button(parent, SWT.PUSH);
 
 	           result.setText("Select Image"); //$NON-NLS-1$
-
+	           
 	           return result;
 
 	   }
@@ -50,4 +53,27 @@ public class FileDialogCellEditor extends DialogCellEditor  {
 	       return rsrcClass.getResource(name).getPath();
 
 	   }
+
+	@Override
+	protected void initButtons() {
+        IPropertyAction clearAction = new IPropertyAction() {
+            public Object execute(Control owner) {
+        		return "";                
+            }
+        };
+        addButton("clear", clearAction); //$NON-NLS-1$
+        IPropertyAction setAction = new IPropertyAction() {
+            public Object execute(Control owner) {
+            	
+        		FileDialog fileDialog = new FileDialog(owner.getShell(), SWT.OPEN);
+        		fileDialog.setFileName("选择图形文件");
+        		fileDialog.setFilterExtensions(new String[] { "*.gif;*.png;*.jpg" , "*.*" });
+        		String path = fileDialog.open();
+        		return path;                
+            }
+        };
+        addButton("select image", setAction);
+        
+		
+	}
 }
